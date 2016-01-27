967a2415cfd80511fc0c76d7232952aa77188085
bynikiyo <nikiyo9@gmail.com>
2016-01-27 14:26:43 +0100

    Insertados nuevos xml con las maestrias y metodo flipView

--- app/src/main/java/bynikiyo/sdowlol/Masteries.java
+++ app/src/main/java/bynikiyo/sdowlol/Masteries.java
@@ -1,19 +1,15 @@
 package bynikiyo.sdowlol;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.io.File;
+import android.widget.ViewFlipper;
 
 public class Masteries extends AppCompatActivity {
-
+    public float init_x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +18,51 @@ public class Masteries extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        final ViewFlipper flipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+        flipper.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        init_x = event.getX();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        float distance = init_x - event.getX();
+
+                        if (distance < 0) {
+
+                            flipper.showPrevious();
+                        }
+
+                        if (distance > 0) {
+
+                            flipper.showNext();
+                        }
+                    default:
+                        break;
+                }
 
 
+                return false;
+            }
+        });
+
+        ImageView fury = (ImageView) findViewById(R.id.fury);
+        ImageView sorcery = (ImageView) findViewById(R.id.sorcery);
+        ImageView dobleSword = (ImageView) findViewById(R.id.dobleSword);
+        ImageView feast = (ImageView) findViewById(R.id.feast);
+        ImageView vampirism = (ImageView) findViewById(R.id.vampirism);
+        ImageView naturalTalent = (ImageView) findViewById(R.id.naturalTalent);
 
 
-
-        ImageView myImage = (ImageView) findViewById(R.id.imageviewTest);
-        myImage.setImageResource(R.drawable.gray_6111);
-
+        fury.setImageResource(R.drawable.m_gray_6111);
+        sorcery.setImageResource(R.drawable.m_gray_6114);
+        dobleSword.setImageResource(R.drawable.m_gray_6121);
+        feast.setImageResource(R.drawable.m_gray_6122);
+        vampirism.setImageResource(R.drawable.m_gray_6131);
+        naturalTalent.setImageResource(R.drawable.m_gray_6134);
     }
     }
 
