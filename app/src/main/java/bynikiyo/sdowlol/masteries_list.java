4cb10f5c46168fe8fb94a6b5afa0770dd65d651c
bynikiyo <nikiyo9@gmail.com>
2016-02-11 13:58:25 +0100

    Eliminada actividad Masteries, ahora todo el contenido esta en list_masteries

--- app/src/main/java/bynikiyo/sdowlol/masteries_list.java
+++ app/src/main/java/bynikiyo/sdowlol/masteries_list.java
@@ -6,9 +6,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import net.rithms.riot.api.RiotApi;
@@ -24,6 +26,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import android.widget.AdapterView.*;
+import android.widget.ViewFlipper;
+
 public class masteries_list extends AppCompatActivity {
         private ListView listaAndroid;
 
@@ -33,6 +37,8 @@ public class masteries_list extends AppCompatActivity {
         private Summoner summoner;
         private MasteryPages p;
         private Set<MasteryPage> pages;
+        private float init_x;
+
 
 
     @Override
@@ -40,7 +46,9 @@ public class masteries_list extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_masteries_list);
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final ViewFlipper flipper = (ViewFlipper) findViewById(R.id.viewFlipper);
         setSupportActionBar(toolbar);
+        flipper.setVisibility(View.INVISIBLE);
 
         /**
          * Getting data from infoSummoner Activity
@@ -204,12 +212,82 @@ public class masteries_list extends AppCompatActivity {
                                     int position, long id) {
 
                 String value = (String)adapter.getItem(position);
+                List<Mastery> lm = getListMastery(value);
 
-                Intent i = new Intent(masteries_list.this,Masteries.class);
-                    i.putExtra("namePage",value);
-                startActivity(i);
+                //------------------------------------------------------------------------
 
-                }
+                flipper.setVisibility(View.VISIBLE);
+                flipper.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+
+                        switch (event.getAction()) {
+
+                            case MotionEvent.ACTION_DOWN:
+                                init_x = event.getX();
+                                return true;
+                            case MotionEvent.ACTION_UP:
+                                float distance = init_x - event.getX();
+
+                                if (distance < 0) {
+
+                                    flipper.showPrevious();
+                                }
+
+                                if (distance > 0) {
+
+                                    flipper.showNext();
+                                }
+                            default:
+                                break;
+                        }
+
+
+                        return false;
+                    }
+                });
+
+
+                /**
+                 * retrieving masterie namePage from the masteries_list activity
+                 */
+
+
+
+                ImageView fury = (ImageView) findViewById(R.id.fury);
+                ImageView sorcery = (ImageView) findViewById(R.id.sorcery);
+                ImageView dobleSword = (ImageView) findViewById(R.id.dobleSword);
+                ImageView feast = (ImageView) findViewById(R.id.feast);
+                ImageView vampirism = (ImageView) findViewById(R.id.vampirism);
+                ImageView naturalTalent = (ImageView) findViewById(R.id.naturalTalent);
+                ImageView bBlow = (ImageView) findViewById(R.id.Bblow);
+                ImageView piercing = (ImageView) findViewById(R.id.Pircing);
+                ImageView warlord = (ImageView) findViewById(R.id.warlord);
+                ImageView fervor = (ImageView) findViewById(R.id.fervor);
+                ImageView deadTouch = (ImageView) findViewById(R.id.deadTouch);
+
+
+                /**
+                 * Gray masteries load
+                 */
+                fury.setImageResource(R.drawable.m_gray_6111);
+                sorcery.setImageResource(R.drawable.m_gray_6114);
+                dobleSword.setImageResource(R.drawable.m_gray_6121);
+                feast.setImageResource(R.drawable.m_gray_6122);
+                vampirism.setImageResource(R.drawable.m_gray_6131);
+                naturalTalent.setImageResource(R.drawable.m_gray_6134);
+                bBlow.setImageResource(R.drawable.m_gray_6151);
+                piercing.setImageResource(R.drawable.m_gray_6154);
+                warlord.setImageResource(R.drawable.m_gray_6161);
+                fervor.setImageResource(R.drawable.m_gray_6162);
+                deadTouch.setImageResource(R.drawable.m_gray_6164);
+
+            }
+
+                //-------------------
+
+
+
 
 
 
